407cfcbcbab8d53a772e2acefa4cccc011ba2efa
dci <rudeshdhote@gmail.com>
2021-03-29 13:46:39 +0200

    initial commit

---
+++ start.js
@@ -0,0 +1,16 @@
+let firstName;
+
+firstName = "Rudesh";
+
+lastName = "Dhote";
+
+console.log(firstName, lastName);
+
+city = " Berlin ";
+
+institute = " DCI ";
+
+hobby = " Taveling ";
+
+console.log("Lives in" + city + "studing in " + institute + "and my hobby is" + hobby)
+
